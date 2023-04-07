f25c51a405aa27e01040ec62170db22aaed628af
thenabiev <sahinnabee@gmail.com>
2023-04-08 03:00:46 +0400

    beta

--- components/section2.js
+++ components/section2.js
@@ -3,9 +3,11 @@ import Link from 'next/link';
 import React from 'react'
 import Author from './_child/author';
 import getPost from '@/lib/helper';
+import fetcher from '@/lib/fetcher';
 
 const Section2 = () => {
-    getPost(2).then(res=>console.log(res))
+    const {data, isLoading, isError}=fetcher('api/posts');
+    if(data) console.log(data);
   return (
     <section className='container mx-auto md:px-20 py-10 '>
         <h1 className='font-bold text-4xl py-12 text-center'>Latest Posts</h1>
